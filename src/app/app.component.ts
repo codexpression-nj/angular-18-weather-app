cd8d5f05501c2bd2268d094698845bd067b6b593
codexpression-nj <n.codexpression@gmail.com>
2024-07-19 23:46:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,121 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './services/weather.service';
+import { Subscription, first } from 'rxjs';
+import { NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgSwitch,NgSwitchCase,FormsModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ 
   title = 'angular-weather-app';
+  cityName: any 
+  city: any;
+  temp: any;
+  hum: any;
+  wind: any;
+  today: any;
+  state :any;
+  daysForecast: any;
+  minTemp:any
+  maxTemp:any
+  // cityIllustrationPath: ansy;
+
+  day1Name: string | undefined;
+  day1State: string | undefined;
+  day1Temp!: number;
+
+
+  day2Name!: string;
+  day2State!: string;
+  day2Temp!: number;
+
+  day3Name!: string;
+  day3State!: string;
+  day3Temp!: number;
+
+  day4Name!: string;
+  day4State!: string;
+  day4Temp!: number;
+
+  day5Name!: string;
+  day5State!: string;
+  day5Temp!: number;
+
+  cityIllustrationPath = "assets/bg1.jpg"
+  sub5: Subscription | undefined;
+
+  errorMessage: any;
+  constructor(private weatherService:WeatherService){}
+ fetchWeather(city:string){
+  console.log(city);
+  
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  const todayNumberInWeek = new Date().getDay();
+  this.weatherService.getweather('pretoria').pipe(first()).subscribe((play) => {
+    console.log(play);
+    
+    this.cityName = play.name
+    this.state = play.weather[0].main
+    this.temp = Math.round(play.main.temp)
+    this.maxTemp = play.main.temp_max
+    this.minTemp = play.main.temp_min
+    this.hum = play.main.humidity
+    this.wind = play.wind.speed
+    
+    
+  }, (err) =>{
+    console.log(err);
+    
+  })
+  this.sub5 = this.weatherService.getForcast('pretoria').pipe(first()).subscribe((data) => {
+    
+    for (let i = 0; i < data.length; i++) {
+      const date = new Date(data[i].dt_txt).getDay();
+      console.log(days[date]);
+      if (((date === todayNumberInWeek + 1) || (todayNumberInWeek === 6 && date === 0)) && !this.day1Name) {
+        this.day1Name = days[date];
+        this.day1State = data[i].weather[0].main;
+        this.day1Temp = Math.round(data[i].main.temp);
+
+      } else if (!!this.day1Name && !this.day2Name && days[date] !== this.day1Name) {
+        this.day2Name = days[date];
+        this.day2State = data[i].weather[0].main;
+        this.day2Temp = Math.round(data[i].main.temp);
+
+      } else if (!!this.day2Name && !this.day3Name && days[date] !== this.day2Name) {
+        this.day3Name = days[date];
+        this.day3State = data[i].weather[0].main;
+        this.day3Temp = Math.round(data[i].main.temp);
+
+      } else if (!!this.day3Name && !this.day4Name && days[date] !== this.day3Name) {
+        this.day4Name = days[date];
+        this.day4State = data[i].weather[0].main;
+        this.day4Temp = Math.round(data[i].main.temp);
+
+      } else if (!!this.day4Name && !this.day5Name && days[date] !== this.day4Name) {
+        this.day5Name = days[date];
+        this.day5State = data[i].weather[0].main;
+        this.day5Temp = Math.round(data[i].main.temp);
+
+      }
+    }
+    
+  })
+ }
+
+  ngOnInit() {
+    
+   
+
+  }
+  
+
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, first, map, retry } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+import {Subject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private readonly baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  private readonly forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+  private readonly appID = ''; 
+
+  constructor(private http:HttpClient) { }
+
+  getweather(city:string): Observable<any>{
+    return this.http.get(`${this.baseURL}${city}&units=metric&APPID=${this.appID}`).pipe((first()))
+  }
+
+  getForcast(city:string):  Subject<Array<any>> {
+    const dataSubject = new Subject<Array<any>>();
+    this.http.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${this.appID}`)
+      .subscribe((weather: any) => {
+        dataSubject.next(weather.list);
+      });
+    return dataSubject
+  }
+
+}
